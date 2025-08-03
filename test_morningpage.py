dfc46bf24e14ec63563838042b14745ee635dc09
kalin <kalinbobchev@gmail.com>
2025-08-03 17:53:30 +0300

    add_logger_big_change_test

---
+++ test_morningpage.py
@@ -0,0 +1,132 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pytest
+
+
+@pytest.mark.usefixtures("setup", "onetimesetup")
+class MorningexpertTests:
+    def setup_method(self):
+        options = Options()
+        prefs = {
+            "profile.default_content_setting_values.notifications": 2
+        }
+        options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(3)
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def teardown_method(self):
+        self.driver.quit()
+
+
+    def test_youtube(self):
+        original_window = self.driver.current_window_handle
+        first_youtube = self.driver.find_element(By.XPATH, '//*[@id="home"]/ul/li[4]/a')
+        self.driver.execute_script(
+            "window.open(arguments[0].href, 'https://www.youtube.com/channel/UCdXg9VKrpTDo2V5Ys-DDdOA');",
+            first_youtube)
+        time.sleep(3)
+        self.driver.switch_to.window(original_window)
+        time.sleep(2)
+        self.driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+        youtube = self.driver.find_element(By.XPATH, '//*[@id="top"]/footer/div[1]/div/div[1]/ul/li[4]/a')
+        youtube.click()
+        time.sleep(3)
+        bizki = self.wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span')))
+        bizki.click()
+        time.sleep(8)
+
+
+
+    def test_login(self):
+        new_url = "https://web.morningexpert.com/#"
+        self.driver.get(new_url)
+        time.sleep(2)
+        time.sleep(1)
+        login_web_app = self.driver.find_element(By.XPATH, "//a[@id='loginButton' and (text())='Login']")
+        login_web_app.click()
+        time.sleep(2)
+        __username = 'kalinbobchev@gmail.com'
+        __password = 'gnBcvT%4ChZJ'
+        self.driver.find_element(By.XPATH,
+                                 '//*[@id="my-login-screen"]/div/div/div/form/ul/li[1]/div/div/div[2]/input').send_keys(
+            __username)
+        self.driver.find_element(By.XPATH,
+                                 '//*[@id="my-login-screen"]/div/div/div/form/ul/li[2]/div/div/div/div/div[2]/input').send_keys(
+            __password)
+        time.sleep(2)
+        elements = self.driver.find_elements(By.XPATH, "//*[@id='loginButton']")
+        elements[1].click()
+        assert elements
+        time.sleep(2)
+
+    def test_password_change_should_fail(self):
+        __username = 'kalinbobchev@gmail.com'
+        __password = 'gnBcvT%4ChZJ'
+
+        new_url = "https://web.morningexpert.com/#"
+        self.driver.get(new_url)
+        time.sleep(2)
+        self.driver.find_element(By.ID, "loginButton").click()
+        time.sleep(2)
+
+        self.driver.find_element(By.XPATH, '//*[@id="my-login-screen"]//input[@type="email"]').send_keys(__username)
+        self.driver.find_element(By.XPATH, '//*[@id="my-login-screen"]//input[@type="password"]').send_keys(__password)
+        time.sleep(1)
+        self.driver.find_elements(By.ID, "loginButton")[1].click()
+        time.sleep(3)
+
+        self.driver.find_element(By.ID, "profile-tab").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, '//*[@id="settings-list"]/ul/li[4]/a').click()
+
+        old_pass = self.driver.find_element(By.ID, "oldPass")
+        new_pass = self.driver.find_element(By.ID, "newPass")
+        confirm_pass = self.driver.find_element(By.ID, "newPass2")
+
+        new_password = '4Psdfg()*__ABV'
+        old_pass.send_keys(__password)
+        new_pass.send_keys(new_password)
+        confirm_pass.send_keys(new_password)
+
+        # Тук проверяваме дали има съобщение за грешка
+        time.sleep(4)
+        error_label = self.driver.find_element(By.XPATH, '//*[@id="changepass-form"]/div[4]/div/div/label')
+
+        assert error_label.is_displayed(), "Expected error message for incorrect old password not found."
+        print("❗ BUG: Password works for login but not for change — inconsistent validation.")
+
+    # After login We test the AI input under Shadow-root
+    def test_AI_shadows_root(self):
+        chat_tab = self.driver.find_element(By.XPATH, '//*[@id="chat-tab"]')
+        chat_tab.click()
+        time.sleep(4)
+        shadow_host = self.driver.find_element(By.CSS_SELECTOR, 'deep-chat')
+        shadow_root = self.driver.execute_script('return arguments[0].shadowRoot', shadow_host)
+        text_input = shadow_root.find_element(By.CSS_SELECTOR, '#text-input')
+        text_input.send_keys('resent news')
+        submit_btn = shadow_root.find_element(By.CSS_SELECTOR, '#submit-icon')
+        submit_btn.click()
+        time.sleep(10)
+
+    #test_AI_shadows_root()
+    def test_tv_sess(self):
+        self.driver.find_element(By.XPATH, '//*[@id="tv-tab"]').click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, '//*[@id="tv"]/div[1]/div[2]/div[3]/div/div/a[3]').click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, '//*[@id="tab-tv"]/div[2]/ul/div[2]/div').click()
+        time.sleep(4)
+
+
+
+
+    def finish_testing(self):
+        self.driver.quit()
+
